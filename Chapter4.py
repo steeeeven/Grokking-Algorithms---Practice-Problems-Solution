ba1ca9f58b89957e8cd12dcd0713ece0da152e20
steeeeven <goldenmonkey421@gmail.com>
2021-01-16 16:41:12 -0800

    Update Chapter4.py

--- Chapter4.py
+++ Chapter4.py
@@ -9,6 +9,15 @@ def sum(arr):
     # print(val)
     # print(f"{arr} \n")
     return val + sum(arr)
-    
 array = [5,3,7,3,8,20]
 print(sum(array))
+
+
+# Problem 4.2
+def count(list):
+  if list == []:
+    return 0
+  return 1+count(list[1:])
+
+list = [2,6,3,9,0,2,1]
+print(count(list))
